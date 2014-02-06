c994af2cd2bd063d7960f7c722932098e318e5fe
Chance Hudson <jchancehud@gmail.com>
2014-02-05 19:45:53 -0600

    multiplayer dickbutts

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-var http = require('http');
+var io = require('socket.io').listen(8080);
 
-var server = http.createServer();
+io.sockets.on('connection', function (socket){
+	socket.emit('connected', socket.id);
+	socket.on('updatePosition', function (position) {
+		socket.position = position;
+		broadcastPositions();
+	});
+});
 
-function handleRequest(req, res){
-	res.writeHead(200, {'content-type': 'text/plain'});
-	res.write('TEST BITCH');
-	res.end();
+function broadcastPositions(){
+	var broadcastArr = new Array();
+	for (var x in io.sockets.sockets){
+		broadcastArr.push(new PositionObject(io.sockets.sockets[x].id, io.sockets.sockets[x].position));
+	}
+	io.sockets.volatile.emit('update', broadcastArr);
 }
 
-server.on('request', handleRequest);
+function PositionObject(id, position){
+	this.id = id;
+	this.position = position;
+}
 
-server.listen(8080);
+io.sockets.on('disconnect', function (socket){
+	
+});
+
--- test.js
+++ test.js
@@ -4,8 +4,54 @@ gamvas.event.addOnLoad(function() {
 });
 
 testState = gamvas.State.extend({
+	init: function () {
+		this.socket = io.connect('http://localhost:8080');
+		this.socket.on('connected', function (id) {
+			this.socketID = id;
+		});
+		var p = new Array();
+		this.players = p;
+		this.socket.on('update', function (updateArr) {
+			if(updateArr.length-1 != p.length){
+				if(updateArr.length-1 > p.length){
+					for(var x=p.length; x<updateArr.length-1; x++){
+						p.push(new testActor('player', 0,0));
+					}
+				}
+				else{
+					p.splice(updateArr.length-1, p.length-updateArr.length-1);
+				}
+			}
+			var c = 0;
+			for(var x in updateArr){
+				if(updateArr[x].id != this.socketID){
+					p[c].position = updateArr[x].position;
+					c++;
+				}
+			}
+		});
+		
+		this.mainActor = new testActor('main', 0,0);
+	},
+	onMouseMove: function (x, y) {
+		var worldMouse = this.camera.toWorld(x,y);
+		this.mainActor.position = new gamvas.Vector2D(worldMouse.x-this.mainActor.width/2.0, worldMouse.y-this.mainActor.height/2.0);
+		this.socket.emit('updatePosition',this.mainActor.position); 
+	},
 	draw: function(t) {
-		this.c.fillStyle = '#fff';
-		this.c.fillText("test text", 0, 0);
+		for(var x in this.players){
+			this.players[x].draw(t);
+		}
+		this.mainActor.draw(t);
+	}
+});
+
+testActor = gamvas.Actor.extend({
+	create: function (name, x, y) {
+		this._super(name, x, y);
+		var st = gamvas.state.getCurrentState();
+		this.width = 128;
+		this.height = 128;
+		this.setFile(st.resource.getImage('res/sprite.png'), 128,128,1,1);
 	}
 });
