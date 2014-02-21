df0b3c4e497cb76ad82512d8977adf63ac54b4b1
Chance Hudson <jchancehud@gmail.com>
2014-02-21 01:05:38 -0600

    added simple chat code

--- src/js/server/chat_server.js
+++ src/js/server/chat_server.js
@@ -1,23 +1,13 @@
 var io = require('socket.io').listen(8080);
 
-var broadcastObj = {};
-var newBroadcastObj = new Array();
-
-var lastUpdate = 0;
-
 io.sockets.on('connection', function (socket){
 	socket.emit('connected', socket.id);
 	socket.on('disconnect', function(arg) {
 		console.log('disconnect');
-		broadcastObj[socket.id] = null;
 	});
-	socket.on('updatePosition', function (position) {
-		broadcastObj[socket.id] = position;
-		var time = new Date().getTime();
-		if(time-lastUpdate > 33){
-			lastUpdate = time;
-			io.sockets.emit('update', broadcastObj);
-		}
+	socket.on('sentMessage', function (message) {
+		message.id = socket.id;
+		io.sockets.emit('message', message);
 	});
 });
 
